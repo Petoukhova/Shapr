662d169d565f9d73a18cd5a931a9c85713270414
Helene Petoukhova <helene-7@mail.ru>
2022-12-02 00:46:59 +0500

    0 Task

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear ();
+Console.WriteLine("Enter the number squared");
+
+int q = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the number being checked");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n*n == q)
+    Console.WriteLine("Profit!");
+else
+    Console.WriteLine("Mistake....");
